88fd19c5a58b6d3ea82974827a288a380cc6329c
patricia <patriciaannecastillo0225@gmail.com>
2024-12-10 23:10:29 +0800

    1st

---
+++ includes/bd-package.php
@@ -0,0 +1,5 @@
+<section class="blog-detail-package">
+<div class="bd-package-container">
+    <img src="images/blog-img-1.png" alt="Pandemic Blog Image"/>
+</div>
+</section>
---
+++ includes/bd-title.php
@@ -0,0 +1,12 @@
+<article>
+    <h1>Blog</h1>
+    <p>
+    Welcome to DEOVAL Printing Services, your 
+    trusted partner for high-quality printing 
+    solutions in Manila. We specialize in a broad 
+    range of services, from offset printing to 
+    risograph, catering to commercial, industrial, 
+    and educational clients.
+    </p>
+</article>
+
